6a9108f0d2b9ba61e935965cfb9f04a7b5e00922
Yana <horoshavina_yana@mail.ru>
2023-03-10 16:44:34 +1200

    Задача 2

---
+++ HW42/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int GetIncomingData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetSum(int Value)
+{
+    int Sum = 0;
+    int Numc;
+    int ValueTMP = Value;
+    while (ValueTMP > 0)
+    {
+        Numc = ValueTMP % 10;
+        Sum = Sum + Numc;
+        ValueTMP = ValueTMP / 10;
+    }
+    return Sum;
+}
+
+int Value = GetIncomingData("Введите число");
+
+Console.WriteLine(GetSum(Value));
